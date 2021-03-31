63b2fae841da00c99802b0648483c537f077c08f
Wescley Klein Frohlich <wescley.kf@gmail.com>
2021-03-31 15:54:14 -0300

    APP Web - Angular

--- ProAgil.API/Startup.cs
+++ ProAgil.API/Startup.cs
@@ -28,6 +28,7 @@ namespace ProAgil.API
         {
             services.AddDbContext<DataContext>(x => x.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
             services.AddControllers();
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -43,7 +44,7 @@ namespace ProAgil.API
             app.UseRouting();
 
             app.UseAuthorization();
-
+            app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
