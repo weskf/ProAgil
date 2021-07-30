540f51a4b6cd645c4a88e5c6d54147ebc9965437
Wescley Frohlich <wescley.kf@gmail.com>
2021-07-29 21:13:03 -0300

    ProAgilRepository

---
+++ ProAgil.Repository/ProAgilContext.cs
@@ -0,0 +1,21 @@
+using Microsoft.EntityFrameworkCore;
+using ProAgil.Domain;
+
+namespace ProAgil.Repository
+{
+    public class ProAgilContext: DbContext
+    {
+        public ProAgilContext(DbContextOptions<ProAgilContext> options) : base(options) {}
+        
+        public DbSet<Evento> Eventos { get; set; }  
+        public DbSet<Palestrante> Palestrantes { get; set; }   
+        public DbSet<RedeSocial> RedeSocials { get; set; }   
+        public DbSet<Lote> Lotes { get; set; }   
+        public DbSet<PalestranteEvento> PalestrantesEventos { get; set; }     
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder){
+            modelBuilder.Entity<PalestranteEvento>()
+            .HasKey(pe => new {pe.Evento, pe.PalestranteId});
+        }
+    }
+}
