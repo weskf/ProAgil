fe1aa7fda02e5e1bcfcf353cdc2d6a8cf765763c
Wescley Frohlich <wescley.kf@gmail.com>
2021-08-16 21:09:33 -0300

    Reactive Forms

--- ProAgil-App/src/app/app.module.ts
+++ ProAgil-App/src/app/app.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser'; 
 import { HttpClientModule } from '@angular/common/http';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EventosComponent } from './eventos/eventos.component';
 import { NavComponent } from './nav/nav.component';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
 import { TooltipModule } from 'ngx-bootstrap/tooltip';
 import { ModalModule } from 'ngx-bootstrap/modal';
 import { DateTimeFormatPipePipe } from './_helps/DateTimeFormatPipe.pipe';
@@ -23,12 +24,14 @@ import { EventoService } from './_services/evento.service';
   imports: [
     BrowserAnimationsModule,
     BsDropdownModule.forRoot(),
+    BsDatepickerModule.forRoot(),
     TooltipModule.forRoot(),
     ModalModule.forRoot(),
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [
     EventoService
--- ProAgil-App/src/app/eventos/eventos.component.ts
+++ ProAgil-App/src/app/eventos/eventos.component.ts
@@ -1,8 +1,12 @@
-import { HttpClient } from '@angular/common/http';
-import { Component, OnInit, TemplateRef } from '@angular/core';
-import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { BsModalService } from 'ngx-bootstrap/modal';
 import { Evento } from '../_models/Evento';
 import { EventoService } from '../_services/evento.service';
+import { ptBrLocale } from 'ngx-bootstrap/locale';
+import { BsLocaleService} from 'ngx-bootstrap/datepicker';
+import { defineLocale } from 'ngx-bootstrap/chronos';
+defineLocale('pt-br', ptBrLocale);
 
 @Component({
   selector: 'app-eventos',
@@ -10,12 +14,36 @@ import { EventoService } from '../_services/evento.service';
   styleUrls: ['./eventos.component.css']
 })
 export class EventosComponent implements OnInit {
+
+  eventoForm = new FormGroup({
+    tema: new FormControl(),
+    local: new FormControl(),
+    dataEvento: new FormControl(),
+    qtdPessoa: new FormControl(),
+    imagemURL: new FormControl(),
+    telefone: new FormControl(),
+    email: new FormControl(),
+  });
+
   eventosFiltrados!: Evento[];
   eventos!: Evento[];
+  evento!: Evento;
   imagemLargura = 50;
   imagemMargem = 2;
   mostrarImagem = false;
-  modalRef!: BsModalRef;
+  
+  constructor(
+    private eventoService: EventoService
+  , private modalService: BsModalService
+  , private fb: FormBuilder
+  , private localService: BsLocaleService) {
+    this.localService.use('pt-br');  
+    this.validation();  
+  }
+
+  ngOnInit() {
+    this.getEventos();
+  }
 
   _filtroLista: string = "";
 
@@ -28,18 +56,27 @@ export class EventosComponent implements OnInit {
     this.eventosFiltrados = this.filtroLista ? this.filtrarEventos(this.filtroLista) : this.eventos;
   }
 
-  constructor(private eventoService: EventoService
-    , private modalService: BsModalService) {}
-
-  openModal(template: TemplateRef<any>){
-    this.modalRef = this.modalService.show(template);
-  }
-  ngOnInit() {
-    this.getEventos();
-  }
+  openModal(template: any){
+    template.show(template);
+  }  
 
   alternarImagem(){
     this.mostrarImagem = !this.mostrarImagem;
+  }
+
+  salvarAlteracao(){
+  }
+
+  public validation() {
+    this.eventoForm = this.fb.group({
+      tema: ['', [Validators.required, Validators.minLength(4), Validators.maxLength(50)]],
+      local: ['', Validators.required],
+      dataEvento: ['', Validators.required],
+      qtdPessoa: ['', [Validators.required, Validators.max(120000)]],
+      imagemURL: ['', Validators.required],
+      telefone:['', Validators.required],
+      email:['', [Validators.required, Validators.email]]
+    });
   }
 
   filtrarEventos(filtrarPor: string): Evento[] {
@@ -50,12 +87,14 @@ export class EventosComponent implements OnInit {
   }
 
   getEventos(){
+    console.log("metodo eventos");
     this.eventoService.getAllEvento().subscribe((_eventos: Evento[]) => {
-       this.eventos = _eventos;
-       this.eventosFiltrados = this.eventos;
-      console.log(_eventos);
+      console.log("Retornou evento") 
+      this.eventos = _eventos;
+      this.eventosFiltrados = this.eventos;
+       console.log(_eventos);
      }, error => { 
-       console.log(error);
+       console.log("erro ao consultar eventos");
       });
   }
 }
